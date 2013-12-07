2d85b7f4f0d1517eb6c333172aed652d90a3a4be
Blake Taylor <blakefrost@gmail.com>
2013-12-06 17:33:57 -0800

    Setup basic rails 4 project for notebox web interface

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoteboxWeb::Application.config.secret_key_base = '9ac628cb4b71f456a103a81af086ffd08ccc18cc6279f99d7bab5a440932d185a8ae08d0d79a7c9a146f333c830cc516a852c5256dfce4258581f5d15fb08f89'
