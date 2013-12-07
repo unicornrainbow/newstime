13c8cf88bb9a50f9ddba29244623375f04337948
Blake Taylor <blakefrost@gmail.com>
2013-12-07 09:57:43 -0800

    Add note path to output

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,16 @@
 class NotesController < ApplicationController
 
   def index
-    today = Date.today
-    today_path = "#{NOTES_ROOT}/entries/#{today.strftime("%Y/%m/%d")}"
-    todays_entries = Dir.entries(today_path)
-    todays_entries.select! { |name| name =~ /.\.txt/ }
-    @notes = todays_entries.map { |name|
-      markdown = File.read(today_path + "/" + name)
+    date = Date.parse('2013/12/7')
+
+    # Normalize date
+    today_path = "#{NOTES_ROOT}/entries/#{date.strftime("%Y/%m/%d")}"
+    todays_entries = Dir["#{today_path}/*.txt"]
+
+    @notes = todays_entries.map { |path|
+      markdown = File.read(path)
       {
-        name: name,
+        name: path,
         markdown: markdown,
         html: $markdown.render(markdown)
       }
