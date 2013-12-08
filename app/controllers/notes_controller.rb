eadb93495e962889bb4aa58bc28e4f40b02bc789
Blake Taylor <blakefrost@gmail.com>
2013-12-07 16:31:04 -0800

    Only show the relative portion of the path

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,14 +13,24 @@ class NotesController < ApplicationController
     todays_entries = Dir["#{today_path}/*.txt"]
 
     @notes = todays_entries.map { |path|
+      root_path = "/Users/blake/.notes/entries/"
+      web_path = path.match(/#{root_path}(.*)/).try(:captures).first
+
       markdown = File.read(path)
       {
-        name: path,
+        name: web_path,
         markdown: markdown,
         html: $markdown.render(markdown)
       }
     }
     @notes.reverse!
+  end
+
+  def show
+
+    date = Date.today - params[:days_ago].to_i.days
+
+
   end
 
 end
