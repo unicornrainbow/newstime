b298675cb625cd6f22e85436a736133039aa5da3
Blake Taylor <blakefrost@gmail.com>
2013-12-09 13:40:25 -0800

    Improvement ot initilizer, getting close to fast generatoion

--- lib/generators/initializer_generator.rb
+++
@@ -1,6 +0,0 @@
-class InitializerGenerator < Rails::Generators::Base
-  desc "This generator creates an initializer file at config/initializers"
-  def create_initializer_file
-    create_file "config/initializers/initializer.rb", "# Add initialization content here"
-  end
-end
