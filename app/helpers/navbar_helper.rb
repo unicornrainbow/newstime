80a596818b0334f68235701045285dd2aadc8534
Blake Taylor <blakefrost@gmail.com>
2013-12-09 12:17:56 -0800

    One step

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,25 +1,25 @@
 module NavbarHelper
   def navbar_notes_class
-    "active" if path_matches? '/(notes.*)?$'
+    active_path '/(notes.*)?$'
   end
 
   def navbar_images_class
-    "active" if path_matches? '/images'
+    active_path '/images'
   end
 
    def navbar_attachments_class
-    "active" if path_matches? '/attachments'
+    active_path '/attachments'
    end
 
    def navbar_wiki_class
-    "active" if path_matches? '/wiki'
+    active_path '/wiki'
    end
 
    def navbar_bookmarks_class
-    "active" if path_matches? '/bookmarks'
+    active_path '/bookmarks'
    end
 
    def navbar_email_class
-    "active" if path_matches? '/email'
+    active_path '/email'
    end
 end
