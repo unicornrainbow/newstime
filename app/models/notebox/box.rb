1d627f4106ee01a69392569b9f7a7460ff73931f
Blake Taylor <blakefrost@gmail.com>
2013-12-11 13:35:52 -0800

    Implement search, showing first 20 results, no paging yet, but looking good

--- app/models/notebox/box.rb
+++ app/models/notebox/box.rb
@@ -48,10 +48,51 @@ class Notebox::Box
     entries
   end
 
-  def search(keyword)
+  def search(keyword, options={})
     sanitize_command!(keyword)
-    results = `cd "#{@path}" && git grep -i "#{keyword}"`
-    results.split("\n")
+    grep_results = `cd "#{@path}" && git grep -i "#{keyword}"`
+    grep_results = grep_results.split("\n")
+
+    file_paths = grep_results.map { |r| r.match(/^(.*\.txt):(.*)/).try(:captures).try(:first) }
+    file_paths.uniq!
+
+    @results = file_paths.map do |file_path|
+      full_path = "#{NOTES_ROOT}/#{file_path}"
+      path, ext = file_path.match(/(.*)\.(txt)$/).try(:captures)
+
+      created_at = parse_created_at("#{path}.#{ext}")
+
+      markdown = File.read(full_path)
+      front_matter, _markdown = markdown.match(/---((.|\n)*)---((.|\n)*)/).try(:captures)
+      if front_matter
+        #front_matter = YAML.load(front_matter)
+        front_matter = {}
+        markdown = markdown.gsub(/---(.|\n)*---/, '') # Strip front matter
+        title = front_matter["title"]
+      end
+
+      render_checkboxes!(markdown)
+
+      html = $markdown.render(markdown)
+
+      attributes = {
+        path: path,
+        markdown: markdown,
+        html: html,
+        created_at: created_at,
+        formatted_date: created_at.strftime('%A, %B %e %Y'),
+        formatted_time: created_at.strftime('%I:%M %p'),
+        formatted_date_time: created_at.strftime('%A, %B %e, %Y, %l:%M %p'),
+        title: title
+      }
+      OpenStruct.new(attributes)
+    end
+
+    @results = @results.sort_by(&:created_at)
+
+    page = options[:page] || 1
+    page_size = options[:page_size] || 20
+    @results.reverse.take(page*page_size)
   end
 
 private
