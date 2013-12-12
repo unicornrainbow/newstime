d713988448c63b0b202ed3e50ae47e9f813b39de
Blake Taylor <blakefrost@gmail.com>
2013-12-12 09:28:49 -0800

    Handled pressing n

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,10 +1,17 @@
 
 function expandTextarea(id) {
-    var $element = $('.myclass').get(0);
+  var $element = $('.myclass').get(0);
 
-    $element.addEventListener('keyup', function() {
-        this.style.overflow = 'hidden';
-        this.style.height = 0;
-        this.style.height = this.scrollHeight + 'px';
-    }, false);
+  $element.addEventListener('keyup', function() {
+    this.style.overflow = 'hidden';
+    this.style.height = 0;
+    this.style.height = this.scrollHeight + 'px';
+  }, false);
 }
+
+$(document).keypress(function(event){
+  // Automatically take the user to new page when pressing n
+  if (event.which == 110) {
+    window.location = '/new';
+  }
+});
