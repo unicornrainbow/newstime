ca39557b15a3d9873896d8a96b47a3eb525c0c2c
Blake Taylor <blakefrost@gmail.com>
2014-01-08 05:17:06 -0500

    Add image styles and thumbnails

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,11 @@ class Photo
   field :name, type: String
 
   include Mongoid::Paperclip
-  has_mongoid_attached_file :attachment
+  has_mongoid_attached_file :attachment,
+    :styles => {
+      :original => ['1920x1680>', :jpg],
+      :small    => ['100x100#',   :jpg],
+      :medium   => ['250x250',    :jpg],
+      :large    => ['500x500>',   :jpg]
+    }
 end
