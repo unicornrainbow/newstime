8ad4e8265c12109ba81eda37d80474091e143fa8
Blake Taylor <blakefrost@gmail.com>
2014-01-08 02:51:21 -0500

    Don't listen on 8089

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,7 +27,7 @@ working_directory APP_PATH
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 listen APP_PATH + "/tmp/sockets/unicorn.sock", :backlog => 64
-listen 8089, :tcp_nopush => true
+#listen 8089, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
