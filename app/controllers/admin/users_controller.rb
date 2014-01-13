19c394cb64ddbdbf80241b61d4365df1ed549e41
Blake Taylor <blakefrost@gmail.com>
2014-01-13 17:05:24 -0500

    Make is so you can create and associate users with organizations

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,10 +21,24 @@ class Admin::UsersController < AdminController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to [:admin, @user], notice: "Edition created successfully."
+    else
+      render "new"
+    end
+  end
+
+
 private
 
   def user_params
-    params.require(:user).permit(:name, :organization_id)
+    params.require(:user).permit(:email, :password, :organization_id)
   end
 
 end
