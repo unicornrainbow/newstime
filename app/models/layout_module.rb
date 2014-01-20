6776ce12c5216ff52ce6a86a278cf6d728854b6f
Blake Taylor <blakefrost@gmail.com>
2014-01-20 13:29:59 -0500

    Check point interation on new layout_module system

--- app/models/layout_module.rb
+++ app/models/layout_module.rb
@@ -1,12 +1,17 @@
-class LayoutModule
+# layout_module = LayoutModule.new('sfrecord/v1')
+# template = layout_module.find_template('main')
+# template.render(title: "San Francisco Record")
+
+module LayoutModule
 
   def initialize(name)
     @name = name
+    @layout_module_root = "#{Rails.root}/vendor/layout_modules/#{@name}"
   end
 
   def render
-    template = Tilt.new("#{Rails.root}/vendor/layout_modules/#{@name}/views/main.html.erb")
-    context = LayoutModuleContext.new(title: "San Francisco Record")
+    template = Tilt.new("#{@layout_module_root}/views/main.html.erb")
+    context = Context.new(title: "San Francisco Record")
     template.render(context) do
       %q{
       <div class="row">
@@ -18,24 +23,33 @@ class LayoutModule
     end
   end
 
-end
 
-class LayoutModuleContext
+  class Template
 
-  attr_accessor :title
+    def self.find(name)
 
-  def initialize(opts={})
-    @title = opts.delete(:title)
+    end
+
   end
 
-  def stylesheet_link_tag(name)
-    %Q{<link href="#{name}.css" rel="stylesheet" media="screen" />}
+  class Context
+    attr_accessor :title
+
+    def initialize(opts={})
+      @title = opts.delete(:title)
+    end
+
+    def stylesheet_link_tag(name)
+      %Q{<link href="#{name}.css" rel="stylesheet" media="screen" />}
+    end
+
+    def javascript_include_tag(name)
+      %Q{<script src="#{name}.js" type="text/javascript"></script>}
+    end
+
+    def render(name, *args)
+
+    end
   end
 
-  def javascript_include_tag(name)
-    %Q{<script src="#{name}.js" type="text/javascript"></script>}
-  end
-
-  def render(*args)
-  end
 end
