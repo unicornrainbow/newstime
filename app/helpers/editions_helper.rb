ceb66aa6559cf6ad2eff0f274dbb8715809ce1ae
Blake Taylor <blakefrost@gmail.com>
2014-01-26 22:38:13 -0500

    Specify layout with yield content, one step closer to mergeing into a unified system

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -2,10 +2,25 @@ module EditionsHelper
   attr_reader :title, :layout_module, :sections, :publish_date, :edition, :composing
   attr_accessor :current_layout
 
+  def layouts
+    @layouts ||= []
+  end
+
   def yield_content(&block)
-    view = LayoutModule::CaptureConcat.new(self)
-    content = @template.render(view) { capture(&block) }
-    concat(view.captured_content.html_safe)
+    content = capture(&block)
+
+    _layout = self.layouts.pop
+    if _layout
+      view = LayoutModule::CaptureConcat.new(self)
+      @template.render(view) { content }
+      concat(view.captured_content.html_safe)
+    else
+      concat(content)
+    end
+  end
+
+  def layout(name)
+    self.layouts << layout_module.templates[name]
   end
 
   # Render content within a partial serving as a layout.
