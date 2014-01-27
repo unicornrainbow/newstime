d1f9d5721a5a46569a36929a2ea999c775ec34f6
Blake Taylor <blakefrost@gmail.com>
2014-01-27 15:11:42 -0500

    Add angular to the composing

--- app/assets/javascripts/composer.js
+++ app/assets/javascripts/composer.js
@@ -1,4 +1,13 @@
 //= require jquery
+//= require jquery_ujs
+//
+//= require angular
+//= require_self
+//= require_tree ./templates
+//= require_tree ./controllers
+//= require_tree ./directives
+//
+//= require lib/jquery.easing
 
 $(function() {
   $(".add-page-btn").click(function() {
