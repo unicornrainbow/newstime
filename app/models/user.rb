a2a670ae27f89ce3b1929d77d7854575a67966c0
Blake Taylor <blakefrost@gmail.com>
2014-02-15 17:28:00 -0500

    Make user not registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
