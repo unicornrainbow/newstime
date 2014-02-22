afb822a2b1915b4725474ad67683e4753777480b
Blake Taylor <blakefrost@gmail.com>
2014-02-22 15:39:39 -0500

    Allow captions for photo and video

--- app/models/content/photo_content_item.rb
+++ app/models/content/photo_content_item.rb
@@ -1,5 +1,6 @@
 module Content
   class PhotoContentItem < ContentItem
+    field :caption, type: String
     belongs_to :photo
   end
 end
--- app/models/content/video_content_item.rb
+++ app/models/content/video_content_item.rb
@@ -1,5 +1,6 @@
 module Content
   class VideoContentItem < ContentItem
+    field :caption, type: String
     belongs_to :video
   end
 end
