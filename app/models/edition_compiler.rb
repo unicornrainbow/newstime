5bfa112d78db3ec2d07bd5171d7cff3355993b16
Blake Taylor <blakefrost@gmail.com>
2014-03-25 23:33:24 -0400

    Compile and output js and css

--- app/models/edition_compiler.rb
+++ app/models/edition_compiler.rb
@@ -25,15 +25,45 @@ class EditionCompiler
 
     stylesheet_assets.uniq!
     stylesheet_assets.each do |stylesheet_path|
-      FileUtils.mkdir_p File.expand_path('..', output_dir.join(stylesheet_path))
-      File.write(output_dir.join(stylesheet_path), "stylesheet contents")
-    end
 
+      environment = Sprockets::Environment.new
+      environment.append_path "#{Rails.root}/layouts/#{@edition.layout_name}/stylesheets"
+
+      # Major hack to load bootstrap into this isolated environment courtesy of https://gist.github.com/datenimperator/3668587
+      Bootstrap.load!
+      environment.append_path Compass::Frameworks['bootstrap'].templates_directory + "/../vendor/assets/stylesheets"
+
+      environment.context_class.class_eval do
+        def asset_path(path, options = {})
+          "/assets/#{path}"
+        end
+      end
+
+      path = stylesheet_path.split('/').tap(&:shift).join('/') # Remove leading stylesheet/
+
+      result = environment[path]
+
+      FileUtils.mkdir_p File.expand_path('..', output_dir.join(stylesheet_path))
+      File.write(output_dir.join(stylesheet_path), result)
+    end
 
     javascript_assets.uniq!
     javascript_assets.each do |javascript_path|
+      environment = Sprockets::Environment.new
+      environment.append_path "#{Rails.root}/layouts/#{@edition.layout_name}/javascripts"
+
+      # Hack to load paths for jquery and angular gems
+      environment.append_path Gem.loaded_specs['angularjs-rails'].full_gem_path + "/vendor/assets/javascripts"
+      environment.append_path Gem.loaded_specs['jquery-rails'].full_gem_path + "/vendor/assets/javascripts"
+
+      # Is is a coffee file or a straight js? Need to have this done
+      # automatically with sprockets or something.
+
+      path = javascript_path.split('/').tap(&:shift).join('/') # Remove leading javascript/
+      result = environment[path]
+
       FileUtils.mkdir_p File.expand_path('..', output_dir.join(javascript_path))
-      File.write(output_dir.join(javascript_path), "javascript content")
+      File.write(output_dir.join(javascript_path), result)
     end
 
     # TODO: Collect and render consumed media module assets
