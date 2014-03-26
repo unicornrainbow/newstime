231f29e44f771f3580180a87584da8722eda937a
Blake Taylor <blakefrost@gmail.com>
2014-03-25 23:37:37 -0400

    Consolodate js and css sprockets environment

--- app/models/edition_compiler.rb
+++ app/models/edition_compiler.rb
@@ -11,7 +11,10 @@ class EditionCompiler
 
   def compile!
     output_dir = Rails.root.join('tmp', 'compiled_editions', @edition.id)
+    # Clean output directory
+    #FileUtils.rm_rf output_dir
     FileUtils.mkdir_p output_dir
+
     sections = edition.sections
     stylesheet_assets = []
     javascript_assets = []
@@ -23,42 +26,37 @@ class EditionCompiler
       File.write(output_dir.join(section.path), sc.html)
     end
 
+    environment = Sprockets::Environment.new
+    environment.append_path "#{Rails.root}/layouts/#{@edition.layout_name}/stylesheets"
+    environment.append_path "#{Rails.root}/layouts/#{@edition.layout_name}/javascripts"
+
+    # Major hack to load bootstrap into this isolated environment courtesy of https://gist.github.com/datenimperator/3668587
+    Bootstrap.load!
+    environment.append_path Compass::Frameworks['bootstrap'].templates_directory + "/../vendor/assets/stylesheets"
+
+    environment.context_class.class_eval do
+      def asset_path(path, options = {})
+        "/assets/#{path}"
+      end
+    end
+
+    # Hack to load paths for jquery and angular gems
+    environment.append_path Gem.loaded_specs['angularjs-rails'].full_gem_path + "/vendor/assets/javascripts"
+    environment.append_path Gem.loaded_specs['jquery-rails'].full_gem_path + "/vendor/assets/javascripts"
+
+    # Stylesheets
     stylesheet_assets.uniq!
     stylesheet_assets.each do |stylesheet_path|
-
-      environment = Sprockets::Environment.new
-      environment.append_path "#{Rails.root}/layouts/#{@edition.layout_name}/stylesheets"
-
-      # Major hack to load bootstrap into this isolated environment courtesy of https://gist.github.com/datenimperator/3668587
-      Bootstrap.load!
-      environment.append_path Compass::Frameworks['bootstrap'].templates_directory + "/../vendor/assets/stylesheets"
-
-      environment.context_class.class_eval do
-        def asset_path(path, options = {})
-          "/assets/#{path}"
-        end
-      end
-
       path = stylesheet_path.split('/').tap(&:shift).join('/') # Remove leading stylesheet/
-
       result = environment[path]
 
       FileUtils.mkdir_p File.expand_path('..', output_dir.join(stylesheet_path))
       File.write(output_dir.join(stylesheet_path), result)
     end
 
+    # Javascripts
     javascript_assets.uniq!
     javascript_assets.each do |javascript_path|
-      environment = Sprockets::Environment.new
-      environment.append_path "#{Rails.root}/layouts/#{@edition.layout_name}/javascripts"
-
-      # Hack to load paths for jquery and angular gems
-      environment.append_path Gem.loaded_specs['angularjs-rails'].full_gem_path + "/vendor/assets/javascripts"
-      environment.append_path Gem.loaded_specs['jquery-rails'].full_gem_path + "/vendor/assets/javascripts"
-
-      # Is is a coffee file or a straight js? Need to have this done
-      # automatically with sprockets or something.
-
       path = javascript_path.split('/').tap(&:shift).join('/') # Remove leading javascript/
       result = environment[path]
 
