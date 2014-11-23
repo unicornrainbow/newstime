a1e9d64b64420fd79cd755934675bf201a5a11b2
Blake Taylor <blakefrost@gmail.com>
2014-11-22 21:23:10 -0500

    Fix nul string when trying to start text area

--- app/models/text_area_content_item.rb
+++ app/models/text_area_content_item.rb
@@ -1,6 +1,6 @@
 class TextAreaContentItem < ContentItem
   field :columns,       type: Integer, default: 1 # How many columns to render text as.
-  field :text,          type: String
+  field :text,          type: String, default: ""
   field :rendered_html, type: String
   field :overrun_html,  type: String
   field :rendered_at,   type: DateTime
